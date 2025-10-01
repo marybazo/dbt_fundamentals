44ba9a6c593af6904f2d54c3d936cbf19a6017db
mary <mariia_bazo@epam.com>
2025-10-01 15:12:28 +0000

    add fixs

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,8 @@
 select 
-    id,
+    id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount,
-    created
+    amount / 100 as amount,
+    created as created_at
 from raw.stripe.payments
